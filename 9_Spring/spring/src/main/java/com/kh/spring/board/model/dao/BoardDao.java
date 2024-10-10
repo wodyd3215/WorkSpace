097aad3181f95bb8a99ba53322110100e9ace003
wodyd3215 <wodyd3215@naver.com>
2024-10-10 21:48:18 +0900

    no message

---
+++ 9_Spring/spring/src/main/java/com/kh/spring/board/model/dao/BoardDao.java
@@ -0,0 +1,33 @@
+package com.kh.spring.board.model.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring.board.model.vo.Board;
+import com.kh.spring.common.vo.PageInfo;
+
+@Repository
+public class BoardDao {
+
+	public int selectListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("boardMapper.selectListCount");
+	}
+	
+	public ArrayList<Board> selectList(SqlSessionTemplate sqlSession, PageInfo pi){
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		
+		return (ArrayList)sqlSession.selectList("boardMapper.selectList", null, rowBounds);
+	}
+	
+	public int increaseCount(SqlSessionTemplate sqlSession, int bno) {
+		return sqlSession.update("boardMapper.increaseCount", bno);
+	}
+	
+	public Board selectBoard(SqlSessionTemplate sqlSession, int bno){
+		return sqlSession.selectOne("boardMapper.selectBoard", bno);
+	}
+}
---
+++ 9_Spring/spring/src/main/java/com/kh/spring/board/service/BoardServiceImpl.java
@@ -0,0 +1,44 @@
+package com.kh.spring.board.service;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.spring.board.model.dao.BoardDao;
+import com.kh.spring.board.model.vo.Board;
+import com.kh.spring.common.vo.PageInfo;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService{
+
+	@Autowired
+	private final SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private final BoardDao boardDao;
+	
+	@Override
+	public int selectListCount() {
+		return boardDao.selectListCount(sqlSession);
+	}
+
+	@Override
+	public ArrayList<Board> selectList(PageInfo pi) {
+		return boardDao.selectList(sqlSession, pi);
+	}
+
+	@Override
+	public int increaseCount(int bno) {
+		return boardDao.increaseCount(sqlSession, bno);
+	}
+
+	@Override
+	public Board selectBoard(int bno) {
+		return boardDao.selectBoard(sqlSession, bno);
+	}
+}
